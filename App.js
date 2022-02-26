e7e1170c99f9ed789f7b6d5d3086afdd4a3fd66a
Omar <o.moreta@gmail.com>
2022-02-26 02:54:19 -0500

    render text for the car fixed #1

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        <View style={styles.title}>
+          <Text style={styles.header}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,420</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  title: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: "500",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#5c5e62",
   },
 });
